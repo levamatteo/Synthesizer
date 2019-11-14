320a892e6fb212d0a7e9fec96db547273da1991f
DNAgb <43895611+DNAgb@users.noreply.github.com>
2019-11-13 17:15:50 -0800

    5.1

--- Js/sketch.js
+++ Js/sketch.js
@@ -4,7 +4,7 @@ let osc;
 let pNoise;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   osc = new p5.Oscillator('square');
 createP('Select a type of siren n draw some circles')
   waveFormSelect = createSelect();
@@ -18,12 +18,12 @@ createP('Select a type of siren n draw some circles')
 
 function draw() {
 line(mouseX,mouseY,pmouseX,pmouseY)
-      
-pNoise = noise(frameCount / 20) * 100;
+
+pNoise = noise(frameCount / 5) * 100;
   osc.freq(map(mouseX, 0, width, 60, 1200) + pNoise);
 
   // osc.amp(map(mouseY, 0, height, .2, 0));
-osc.amp(map(sin(frameCount/10),-1, 1, 0.2, .05));
+osc.amp(map(sin(frameCount/3),-1, 1, 0.2, .05));
 }
 function setWaveForm(){
 osc.setType(waveFormSelect.value());
@@ -32,7 +32,7 @@ osc.setType(waveFormSelect.value());
     osc.start();
 
 }
-  
+
   function mouseReleased(){
         osc.stop();
   }
